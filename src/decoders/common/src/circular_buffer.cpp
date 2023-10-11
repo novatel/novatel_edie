534d828bc8e3b9c6d2c89483ea5b845e3285a9a1
Martin Valgur <martin.valgur@gmail.com>
2023-10-12 00:21:51 +0300

    circularbuffer.cpp: new only returns a nullpointer if std::nothrow is used

--- src/decoders/common/src/circular_buffer.cpp
+++ src/decoders/common/src/circular_buffer.cpp
@@ -27,6 +27,7 @@
 #include <algorithm>
 #include <cassert>
 #include <cstring>
+#include <new>
 
 #include "decoders/common/api/circular_buffer.hpp"
 #include "decoders/common/api/nexcept.hpp"
@@ -37,7 +38,7 @@ void CircularBuffer::SetCapacity(const uint32_t uiCapacity_)
     // Set the size of the buffer (bytes)
     if (uiCapacity_ <= uiMyCapacity) { return; }
 
-    const auto pucBuffer = new unsigned char[uiCapacity_];
+    const auto pucBuffer = new (std::nothrow) unsigned char[uiCapacity_];
 
     // Do nothing if new failed.... just use existing buffer
     if (pucBuffer != nullptr)
