5f06eddecf1626d488a253f03492abed3c59f3c6
Martin Valgur <martin.valgur@gmail.com>
2023-10-30 18:50:34 +0200

    Streamline converter_components.py a bit more

--- python/examples/converter_components.py
+++ python/examples/converter_components.py
@@ -44,26 +44,28 @@ def _configure_logging():
     atexit.register(Logging.shutdown)
 
 
-def read_as_frames(input_file, framer):
+def read_frames(input_file, framer):
+    # Write unrecognized and incomplete messages to a separate file.
+    unknown_bytes_stream = ne.OutputFileStream(f"{input_file}.UNKNOWN")
     with open(input_file, "rb") as input_stream:
-        while read_data := input_stream.read(ne.MAX_ASCII_MESSAGE_LENGTH):
+        while read_data := input_stream.read(ne.MESSAGE_SIZE_MAX):
             framer.write(read_data)
             while True:
-                status, frame, metadata = framer.get_frame()
+                status, frame, meta = framer.get_frame()
                 if status in [STATUS.BUFFER_EMPTY, STATUS.INCOMPLETE]:
                     break
-                yield status, frame, metadata
+                if status == STATUS.UNKNOWN or meta.response:
+                    unknown_bytes_stream.write(frame)
+                    continue
+                yield status, frame, meta
+    unknown_bytes_stream.write(framer.flush())
 
 
 def main():
     _configure_logging()
     logger = Logging().register_logger("converter")
 
-    logger.info(f"Decoder library information:\n{ne.pretty_version}")
-
-    parser = argparse.ArgumentParser(
-        description="Convert OEM log files using low-level components."
-    )
+    parser = argparse.ArgumentParser(description="Convert OEM log files using low-level components.")
     parser.add_argument("input_file", help="Input file")
     parser.add_argument(
         "output_format",
@@ -76,6 +78,7 @@ def main():
     args = parser.parse_args()
     encode_format = ne.string_to_encode_format(args.output_format)
 
+    logger.info(f"Decoder library information:\n{ne.pretty_version}")
     if args.version:
         exit(0)
 
@@ -83,45 +86,37 @@ def main():
         logger.error(f'File "{args.input_file}" does not exist')
         exit(1)
 
-    # Setup the EDIE components
+    # Set up the EDIE components
     framer = ne.Framer()
     framer.set_report_unknown_bytes(True)
     framer.set_payload_only(False)
     framer.set_frame_json(False)
-
     header_decoder = ne.HeaderDecoder()
     message_decoder = ne.MessageDecoder()
     encoder = ne.Encoder()
     filter = ne.Filter()
 
-    # Set up file streams
     converted_logs_stream = ne.OutputFileStream(f"{args.input_file}.{encode_format}")
-    unknown_bytes_stream = ne.OutputFileStream(f"{args.input_file}.UNKNOWN")
 
-    for framer_status, frame, meta in read_as_frames(args.input_file, framer):
+    for framer_status, frame, meta in read_frames(args.input_file, framer):
         try:
-            if framer_status == STATUS.UNKNOWN:
-                unknown_bytes_stream.write(frame)
-            else:
-                framer_status.raise_on_error("Framer.get_frame() failed")
-            if meta.response:
-                unknown_bytes_stream.write(frame)
-                continue
+            framer_status.raise_on_error("Framer.get_frame() failed")
             logger.info(f"Framed: {frame}")
 
-            # Decode the header.  Get meta data here and populate the Intermediate header.
+            # Decode the header.
             status, header = header_decoder.decode(frame, meta)
             status.raise_on_error("HeaderDecoder.decode() failed")
 
-            # Filter the log, pass over this log if we don't want it.
+            # Filter the log, pass over it if we don't want it.
             if not filter.do_filtering(meta):
                 continue
 
-            # Decode the Log, pass the meta data and populate the intermediate log.
+            # Decode the log body.
             body = frame[meta.header_length :]
             status, message = message_decoder.decode(body, meta)
             status.raise_on_error("MessageDecoder.decode() failed")
 
+            # Re-encode the log and write it to the output file.
             status, encoded_message = encoder.encode(header, message, meta, encode_format)
             status.raise_on_error("Encoder.encode() failed")
 
@@ -129,10 +124,6 @@ def main():
             logger.info(f"Encoded: ({len(encoded_message.message)}) {encoded_message.message}")
         except ne.DecoderException as e:
             logger.warn(str(e))
-
-    # Clean up
-    unparsed_bytes = framer.flush()
-    unknown_bytes_stream.write(unparsed_bytes)
 
 
 if __name__ == "__main__":
