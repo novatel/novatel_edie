33a09f8e654ac17b7272383817c29774b97cce2a
Martin Valgur <martin.valgur@gmail.com>
2024-05-12 16:38:15 +0300

    Improve run_tests.sh

--- regression/run_tests.sh
+++ regression/run_tests.sh
@@ -3,14 +3,14 @@
 converter_components=${1:-converter_components}
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-if ! command -v "$converter_components"; then
+if ! command -v "$converter_components" > /dev/null; then
   echo "$converter_components not found"
   exit 1
 fi
 
 failures=()
 for FORMAT in "ASCII" "ABBREV_ASCII" "BINARY" "FLATTENED_BINARY" "JSON"; do
-    "$converter_components" "$script_dir/../database/messages_public.json" "$script_dir/BESTUTMBIN.GPS" $FORMAT
+    "$converter_components" "$script_dir/../database/messages_public.json" "$script_dir/BESTUTMBIN.GPS" $FORMAT > /dev/null
     retval=$?
     if [ $retval -ne 0 ]; then
       echo "converter_components failed for $FORMAT failed with status $retval"
@@ -18,10 +18,14 @@ for FORMAT in "ASCII" "ABBREV_ASCII" "BINARY" "FLATTENED_BINARY" "JSON"; do
 
     if ! cmp -s "$script_dir/BESTUTMBIN.GPS.$FORMAT" "$script_dir/targets/BESTUTMBIN.GPS.$FORMAT"; then
         failures+=("OEM-$FORMAT")
+    else
+      rm "$script_dir/BESTUTMBIN.GPS.$FORMAT"
     fi
     
     if ! cmp -s "$script_dir/BESTUTMBIN.GPS.$FORMAT.UNKNOWN" "$script_dir/targets/BESTUTMBIN.GPS.$FORMAT.UNKNOWN"; then
         failures+=("OEM-$FORMAT.UNKNOWN")
+    else
+      rm "$script_dir/BESTUTMBIN.GPS.$FORMAT.UNKNOWN"
     fi
 done
 
@@ -29,3 +33,4 @@ if [ ${#failures} -gt 0 ]; then
     echo "One or more regression tests failed! ${failures[*]}"
     exit 1
 fi
+
