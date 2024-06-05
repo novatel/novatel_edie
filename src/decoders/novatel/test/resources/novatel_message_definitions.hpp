3e39e1e44aa516bebf9fb19bb939ee47ff495c4f
zainarnoldhexagon <167124483+zainarnoldhexagon@users.noreply.github.com>
2024-06-05 13:35:55 -0600

    Update novatel_message_definitions.hpp

--- src/decoders/novatel/test/resources/novatel_message_definitions.hpp
+++ src/decoders/novatel/test/resources/novatel_message_definitions.hpp
@@ -1,7 +1,7 @@
 // clang-format off
 // ===============================================================================
 // |                                                                             |
-// |  COPYRIGHT NovAtel Inc, 2022. All rights reserved.                          |
+// |  COPYRIGHT NovAtel Inc, 2024. All rights reserved.                          |
 // |                                                                             |
 // |  Permission is hereby granted, free of charge, to any person obtaining a    |
 // |  copy of this software and associated documentation files (the "Software"), |
