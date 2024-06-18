f12eb2fbbfc2277de6d5effdcdb4a831aae347ab
Jonathan McDermid <jonathan.mcdermid1@ucalgary.ca>
2024-06-18 16:06:23 -0600

    Update src/decoders/dynamic_library/src/novatel_framer.cpp

--- src/decoders/dynamic_library/src/novatel_framer.cpp
+++ src/decoders/dynamic_library/src/novatel_framer.cpp
@@ -24,9 +24,9 @@
 // ! \file novatel_framer.cpp
 // ===============================================================================
 
-#include "novatel_framer.hpp"
-
 #include <limits>
+
+#include "novatel_framer.hpp"
 
 using namespace novatel::edie;
 using namespace novatel::edie::oem;
