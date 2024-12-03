369b6ab41ab836556b708bfc4807fa0847511d5f
Jonathan McDermid <jonathan.mcdermid1@ucalgary.ca>
2024-12-02 23:09:08 -0700

    Update common.hpp

--- include/novatel_edie/decoders/oem/rangecmp/common.hpp
+++ include/novatel_edie/decoders/oem/rangecmp/common.hpp
@@ -98,16 +98,34 @@ template <typename T> T ExtractBitfield(unsigned char** ppucData_, uint32_t& uiB
     if (uiBitsInBitfield_ > uiBytesLeft_ * 8 - uiBitOffset_) { throw std::runtime_error("Not enough bytes remaining in the buffer."); }
     if (uiBitsInBitfield_ + uiBitOffset_ > 64) { throw std::runtime_error("Bitfield too large for a single read."); }
 
-    const uint64_t mask = (1ULL << uiBitsInBitfield_) - 1;
-    const uint64_t result = *reinterpret_cast<uint64_t*>(*ppucData_) >> uiBitOffset_;
-    const uint32_t uiBitTotal = uiBitOffset_ + uiBitsInBitfield_;
-    const uint32_t uiBytesConsumed = uiBitTotal / 8;
+    uint64_t ullBitfield = 0;
 
-    *ppucData_ += uiBytesConsumed;
-    uiBytesLeft_ -= uiBytesConsumed;
-    uiBitOffset_ = uiBitTotal % 8;
-    
-    return static_cast<T>(mask & result);
+    for (uint32_t uiBitsConsumed = 0; uiBitsConsumed < uiBitsInBitfield_; uiBitsConsumed++)
+    {
+        if ((**ppucData_ & (1UL << uiBitOffset_)) != 0) { ullBitfield |= 1ULL << uiBitsConsumed; }
+
+        if (++uiBitOffset_ == 8)
+        {
+            uiBitOffset_ = 0;
+            --uiBytesLeft_;
+            ++*ppucData_;
+        }
+    }
+
+    return static_cast<T>(ullBitfield);
+
+    // if (uiBitsInBitfield_ + uiBitOffset_ > 64) { throw std::runtime_error("Bitfield too large for a single read."); }
+    // 
+    // const uint64_t mask = (1ULL << uiBitsInBitfield_) - 1;
+    // const uint64_t result = *reinterpret_cast<uint64_t*>(*ppucData_) >> uiBitOffset_;
+    // const uint32_t uiBitTotal = uiBitOffset_ + uiBitsInBitfield_;
+    // const uint32_t uiBytesConsumed = uiBitTotal / 8;
+    // 
+    // *ppucData_ += uiBytesConsumed;
+    // uiBytesLeft_ -= uiBytesConsumed;
+    // uiBitOffset_ = uiBitTotal % 8;
+    // 
+    // return static_cast<T>(mask & result);
 }
 
 //-----------------------------------------------------------------------
