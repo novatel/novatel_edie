bd587d0a74fc5ecc3591c4113267bcb30e701a64
Riley Kinahan <riley.kinahan@hexagon.com>
2025-01-27 16:01:50 -0700

    add submodules

--- python/novatel_edie/__init__.py
+++ python/novatel_edie/__init__.py
@@ -2,17 +2,20 @@ import importlib_resources
 
 from .bindings import (
     messages, enums,
-    MESSAGE_SIZE_MAX, HEADER_FORMAT, ENCODE_FORMAT, STATUS, FIELD_TYPE, DATA_TYPE,
-    TIME_STATUS, MEASUREMENT_SOURCE, OEM4_BINARY_HEADER_LENGTH, MAX_ASCII_MESSAGE_LENGTH,
-    NMEA_SYNC_LENGTH, NMEA_CRC_LENGTH, OEM4_SHORT_BINARY_SYNC_LENGTH, OEM4_SHORT_BINARY_HEADER_LENGTH,
-    OEM4_BINARY_CRC_LENGTH, OEM4_SHORT_ASCII_SYNC_LENGTH, OEM4_ASCII_CRC_LENGTH, MAX_SHORT_ASCII_MESSAGE_LENGTH,
-    OEM4_BINARY_SYNC_LENGTH, MAX_BINARY_MESSAGE_LENGTH, OEM4_ASCII_SYNC_LENGTH, OEM4_BINARY_SYNC1, OEM4_BINARY_SYNC2,
-    OEM4_BINARY_SYNC3,
-    string_to_encode_format, pretty_version, get_default_database, MessageDatabase,
-    Oem4BinaryHeader, Oem4BinaryShortHeader,
-    MetaData, MessageData, Commander, JsonDbReader, BaseField, RangeDecompressor,
-    Framer, HeaderDecoder, MessageDecoder, Encoder, Filter, DecoderException, Logging, LogLevel,
-    Parser, RxConfigHandler, FileParser)
+    HEADER_FORMAT, ENCODE_FORMAT, STATUS, FIELD_TYPE, DATA_TYPE, TIME_STATUS, MEASUREMENT_SOURCE,
+    MESSAGE_SIZE_MAX, MAX_ASCII_MESSAGE_LENGTH, MAX_SHORT_ASCII_MESSAGE_LENGTH, MAX_BINARY_MESSAGE_LENGTH,
+    NMEA_SYNC_LENGTH, NMEA_CRC_LENGTH,
+    OEM4_BINARY_HEADER_LENGTH, OEM4_SHORT_BINARY_HEADER_LENGTH,
+    OEM4_BINARY_CRC_LENGTH, OEM4_ASCII_CRC_LENGTH,
+    OEM4_BINARY_SYNC_LENGTH, OEM4_SHORT_ASCII_SYNC_LENGTH, OEM4_ASCII_SYNC_LENGTH, OEM4_SHORT_BINARY_SYNC_LENGTH,
+    OEM4_BINARY_SYNC1, OEM4_BINARY_SYNC2, OEM4_BINARY_SYNC3,
+    string_to_encode_format, pretty_version,
+    JsonDbReader, MessageDatabase, get_default_database,
+    Oem4BinaryHeader, Oem4BinaryShortHeader, MetaData, MessageData, BaseField,
+    Framer, Filter, HeaderDecoder, MessageDecoder, DecoderException, Encoder, Commander, Parser, FileParser,
+    RangeDecompressor, RxConfigHandler,
+    Logging, LogLevel,
+)
 
 def default_json_db_path():
     """Returns a context manager that yields the path to the default JSON database."""
