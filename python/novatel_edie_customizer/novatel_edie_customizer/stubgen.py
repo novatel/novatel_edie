6ef506d2855fb6777a89b423ec23608fc21d4b4b
Riley Kinahan <riley.kinahan@hexagon.com>
2025-02-06 11:22:17 -0700

    nicer text formatting

--- python/novatel_edie_customizer/novatel_edie_customizer/stubgen.py
+++ python/novatel_edie_customizer/novatel_edie_customizer/stubgen.py
@@ -26,6 +26,7 @@ A module concerning the generation of type hint stub files for the novatel_edie 
 import os
 import json
 import re
+import textwrap
 from typing import Union
 
 import typer
@@ -185,11 +186,14 @@ class StubGenerator:
         subfield_hints = []
 
         # Create the Message type hint
-        message_hint = (f'class {message_def["name"]}(Message):\n'
-                         '    @property\n'
-                         '    def header(self) -> Header: ...\n\n'
-                         '    @property\n'
-                        f'    def body(self) -> {message_def["name"]}_Body: ...\n\n')
+        name = message_def['name']
+        message_hint = textwrap.dedent(f"""\
+            class {name}(Message):
+                @property
+                def header(self) -> Header: ...
+                @property
+                def body(self) -> {name}_Body: ...
+        """)
 
         # Create the MessageBody type hint
         body_name = f'{message_def["name"]}_Body'
