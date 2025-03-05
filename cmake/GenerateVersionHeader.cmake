c4f4f195abeb4d6b47af3ea86981030ade3d01c5
Riley Kinahan <riley.kinahan@hexagon.com>
2025-03-05 14:23:08 -0700

    Minor fixes

--- cmake/GenerateVersionHeader.cmake
+++ cmake/GenerateVersionHeader.cmake
@@ -5,7 +5,7 @@ get_filename_component(SRC_DIR ${SRC} DIRECTORY)
 if(GIT_EXECUTABLE)
 
   execute_process(
-    COMMAND ${GIT_EXECUTABLE} describe --always --tags --abbrev=0 --match "v*"
+    COMMAND ${GIT_EXECUTABLE} describe --tags --abbrev=0 --match "v*"
     WORKING_DIRECTORY ${SRC_DIR}
     OUTPUT_VARIABLE GIT_RELEASE_VERSION
     RESULT_VARIABLE GIT_RELEASE_VERSION_ERROR_CODE
@@ -13,7 +13,7 @@ if(GIT_EXECUTABLE)
     )
 
   execute_process(
-    COMMAND ${GIT_EXECUTABLE} describe --always --tags --dirty --match "v*"
+    COMMAND ${GIT_EXECUTABLE} describe --tags --dirty --match "v*"
     WORKING_DIRECTORY ${SRC_DIR}
     OUTPUT_VARIABLE GIT_DESCRIBE_VERSION
     RESULT_VARIABLE GIT_DESCRIBE_ERROR_CODE
