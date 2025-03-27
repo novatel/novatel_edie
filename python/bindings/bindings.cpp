3bb47f8c746877d65a1ff4515aa2762aeb84c7d7
Riley Kinahan <riley.kinahan@hexagon.com>
2025-03-27 09:25:16 -0600

    Resort forward defs

--- python/bindings/bindings.cpp
+++ python/bindings/bindings.cpp
@@ -1,5 +1,6 @@
-#include <nanobind/nanobind.h>
 #include <memory>
+
+#include <nanobind/nanobind.h>
 
 #include "novatel_edie/common/logger.hpp"
 
@@ -7,22 +8,22 @@ namespace nb = nanobind;
 
 void init_common_common(nb::module_&);
 void init_common_logger(nb::module_&, nb::module_&);
-void init_message_db_singleton(nb::module_&);
 void init_common_message_database(nb::module_&);
-void init_novatel_commander(nb::module_&);
-void init_novatel_common(nb::module_&);
-void init_novatel_exceptions(nb::module_&);
-void init_novatel_decoder(nb::module_&);
-void init_novatel_file_parser(nb::module_&);
-void init_novatel_filter(nb::module_&);
-void init_novatel_framer(nb::module_&);
+void init_message_db_singleton(nb::module_&);
 void init_header_objects(nb::module_&);
 void init_message_objects(nb::module_&);
-void init_novatel_oem_enums(nb::module_&);
-void init_novatel_oem_messages(nb::module_&);
+void init_novatel_common(nb::module_&);
+void init_novatel_exceptions(nb::module_&);
 void init_novatel_parser(nb::module_&);
+void init_novatel_file_parser(nb::module_&);
+void init_novatel_framer(nb::module_&);
+void init_novatel_decoder(nb::module_&);
+void init_novatel_filter(nb::module_&);
+void init_novatel_commander(nb::module_&);
 void init_novatel_range_decompressor(nb::module_&);
 void init_novatel_rxconfig_handler(nb::module_&);
+void init_novatel_oem_messages(nb::module_&);
+void init_novatel_oem_enums(nb::module_&);
 void init_decoder_tester(nb::module_&);
 void init_logger_tester(nb::module_&);
 
@@ -46,8 +47,8 @@ NB_MODULE(bindings, m)
     init_novatel_decoder(m);
     init_novatel_filter(m);
     init_novatel_commander(m);
-    init_novatel_rxconfig_handler(m);
     init_novatel_range_decompressor(m);
+    init_novatel_rxconfig_handler(m);
     init_novatel_oem_messages(messages_mod);
     init_novatel_oem_enums(enums_mod);
     init_decoder_tester(internal_mod);
