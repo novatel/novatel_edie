6102ea4ec5596de73fa393bc2bc6a6bd8875d776
Pradeep Kumar Karn <pradeepkumar.karn@hexagon.com>
2025-09-30 00:34:58 +0530

    Corrected validation logic for ENUM fields to accept int16_t or int32_t

--- src/decoders/common/src/message_decoder.cpp
+++ src/decoders/common/src/message_decoder.cpp
@@ -71,13 +71,10 @@ bool FieldContainer::Validate() const
         if (!ValidateSimpleField()) { return false; }
         break;
     }
-    case FIELD_TYPE::ENUM:
-    if (!std::visit([](auto&& val) -> bool {
-        using T = std::decay_t<decltype(val)>;
-        return std::is_same_v<T, int32_t> || std::is_same_v<T, int16_t>;
-    }, fieldValue)) return false;
-         break;
-
+    case FIELD_TYPE::ENUM: {
+        if (!(std::holds_alternative<int32_t>(fieldValue) || std::holds_alternative<int16_t>(fieldValue))){return false;}
+        break;
+    }
     case FIELD_TYPE::FIXED_LENGTH_ARRAY: [[fallthrough]];
     case FIELD_TYPE::VARIABLE_LENGTH_ARRAY: {
         if (!std::holds_alternative<std::vector<FieldContainer>>(fieldValue) && !ValidateSimpleField()) { return false; }
