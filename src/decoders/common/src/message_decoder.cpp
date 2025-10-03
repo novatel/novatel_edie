a4f1bd5224a0cd97b6276d21afb320171a2ce7a2
karnpra <PradeepKumar.Karn@hexagon.com>
2025-10-03 21:21:45 +0530

    Corrected validation logic for ENUM fields to accept int16_t or int32_t (#172)
    * Corrected validation logic for ENUM fields to accept int16_t or int32_t
    
    * Corrected validation logic for ENUM fields to accept int16_t or int32_t
    
    * Corrected validation logic for ENUM fields to accept int16_t or int32_t
    
    * Corrected validation logic for ENUM fields to accept int16_t or int32_t
    
    * Corrected validation logic for ENUM fields to accept int16_t or int32_t
    
    * Corrected validation logic for ENUM fields to accept int16_t or int32_t
    
    * Fix for crashes when converting datasets to ASCII using waas-converter-components
    
    * Addessing clang issue
    
    * Clang issue fix

--- src/decoders/common/src/message_decoder.cpp
+++ src/decoders/common/src/message_decoder.cpp
@@ -72,7 +72,7 @@ bool FieldContainer::Validate() const
         break;
     }
     case FIELD_TYPE::ENUM: {
-        if (!std::holds_alternative<int32_t>(fieldValue)) { return false; }
+        if (!(std::holds_alternative<int32_t>(fieldValue) || std::holds_alternative<int16_t>(fieldValue))) { return false; }
         break;
     }
     case FIELD_TYPE::FIXED_LENGTH_ARRAY: [[fallthrough]];
