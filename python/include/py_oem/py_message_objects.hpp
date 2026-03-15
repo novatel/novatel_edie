d4bd48bc2b64debc18ed81b4a28a45d01350a66e
Riley Kinahan <riley.kinahan@hexagon.com>
2026-03-15 12:41:34 -0600

    Remove copy

--- python/include/py_oem/py_message_objects.hpp
+++ python/include/py_oem/py_message_objects.hpp
@@ -126,7 +126,7 @@ struct PyEncodableField : public py_common::PyField
 
     explicit PyEncodableField(std::vector<FieldContainer> fields_, PyMessageDatabase::ConstPtr encoderDb_, PyHeader header_,
                               const MessageDefinition* messageDef_, uint32_t messageCrc_)
-        : PyField(fields_, messageDef_, messageCrc_, std::move(encoderDb_->GetCoreDatabase())), header(std::move(header_)),
+        : PyField(std::move(fields_), messageDef_, messageCrc_, std::move(encoderDb_->GetCoreDatabase())), header(std::move(header_)),
           encoderDb(std::move(encoderDb_)), messageDef(messageDef_), messageCrc(messageCrc_) {};
 
     py_common::PyMessageData encode(ENCODE_FORMAT fmt);
