83067d2b09789633aa119ac9401ee0b1c342f7fd
seanovens <sean.ovens@hexagon.com>
2026-03-16 08:58:17 -0600

    Flush frame buffer in framer benchmarks, fix JSON framer (#211)
    * Flush framer buffer in framer benchmarks
    
    * Fix open brace initialization for JSON framer

--- benchmarks/benchmark.cpp
+++ benchmarks/benchmark.cpp
@@ -107,6 +107,7 @@ template <size_t N> static void Frame(benchmark::State& state, const unsigned ch
     for ([[maybe_unused]] auto _ : state) {
         (void)clFramer.Write(data, sizeof(data));
         (void)clFramer.GetFrame(buffer.data(), static_cast<uint32_t>(buffer.size()), stMetaData);
+        (void)clFramer.Flush(buffer.data(), static_cast<uint32_t>(buffer.size()));
     }
     
     state.counters["logs_per_second"] = benchmark::Counter(state.iterations(), benchmark::Counter::kIsRate);
@@ -122,6 +123,7 @@ template <size_t N> static void FrameManager(benchmark::State& state, const unsi
     for ([[maybe_unused]] auto _ : state) {
         (void)clFramerManager.Write(data, sizeof(data));
         (void)clFramerManager.GetFrame(buffer.data(), static_cast<uint32_t>(buffer.size()), stMetaData);
+        (void)clFramerManager.Flush(buffer.data(), static_cast<uint32_t>(buffer.size()));
     }
 
     state.counters["logs_per_second"] = benchmark::Counter(state.iterations(), benchmark::Counter::kIsRate);
