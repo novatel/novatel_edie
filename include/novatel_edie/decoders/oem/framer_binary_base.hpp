a0db8f80220257f1dba3655807f643ad9e1a5803
Sean Ovens <sean.ovens@hexagon.com>
2026-04-06 15:59:59 -0600

    Add documentation to framer_binary_base.hpp

--- include/novatel_edie/decoders/oem/framer_binary_base.hpp
+++ include/novatel_edie/decoders/oem/framer_binary_base.hpp
@@ -32,22 +32,50 @@
 
 namespace novatel::edie::oem {
 
+//============================================================================
+//! \class FramerBinaryBase
+//! \brief Search bytes for patterns that could be an OEM binary message.
+//============================================================================
 template <HEADER_FORMAT HeaderFormat, size_t HeaderLength, size_t MessageLengthIndex, typename MessageLengthT, size_t MaxMessageLength>
 class FramerBinaryBase : public FramerBase
 {
   protected:
+    //----------------------------------------------------------------------------
+    //! \brief A constructor for the FramerBinaryBase class.
+    //! \param [in] buffer a shared pointer to the framer manager's fixed buffer.
+    //----------------------------------------------------------------------------
     FramerBinaryBase(std::shared_ptr<UCharFixedBuffer> buffer, const std::string& loggerName) : FramerBase(loggerName, buffer) {}
 
+    //----------------------------------------------------------------------------
+    //! \brief A constructor for the FramerBinaryBase class.
+    //----------------------------------------------------------------------------
     FramerBinaryBase(const std::string& loggerName) : FramerBase(loggerName) {}
 
+    //----------------------------------------------------------------------------
+    //! \brief Get the sync bytes that identify this binary message format.
+    //----------------------------------------------------------------------------
     [[nodiscard]] virtual std::array<unsigned char, 3> GetSyncByteArray() const noexcept = 0;
 
+    //----------------------------------------------------------------------------
+    //! \brief Find the index of the first sync byte sequence in the internal buffer.
+    //!
+    //! \return The index of the first sync byte sequence if found, or the minimum of the
+    //!     buffer size and the max lookahead if not found.
+    //----------------------------------------------------------------------------
     size_t FindSync() const override
     {
         const auto syncIndex = pclMyBuffer->search_chars(GetSyncByteArray(), 0, MAX_LOOKAHEAD_BYTES);
         return syncIndex == UCharFixedBuffer::npos ? std::min(pclMyBuffer->size(), MAX_LOOKAHEAD_BYTES) : syncIndex;
     }
 
+    //----------------------------------------------------------------------------
+    //! \brief Find the end of the candidate frame starting from the provided index.
+    //!
+    //! \param[in] start The index from which to start searching for the end of frame.
+    //! \return A FindFrameEndResult struct containing the status, format, and index of the end of the frame.
+    //!
+    //! \see FramerBase::FindFrameEndResult for details on the return struct.
+    //----------------------------------------------------------------------------
     FindFrameEndResult FindFrameEnd([[maybe_unused]] size_t start) const override
     {
         using Result = FindFrameEndResult;
@@ -65,6 +93,13 @@ class FramerBinaryBase : public FramerBase
                                                    : Result{Result::Status::COMPLETE, HeaderFormat, uiTotalMessageLength};
     }
 
+    //----------------------------------------------------------------------------
+    //! \brief Validate the candidate frame ending at the byte preceding the provided index.
+    //!
+    //! \param[in] frameEnd The index following the last byte of the candidate frame to validate.
+    //! \return If the frame is valid, return 0. If the frame is invalid, return the number of
+    //!     bytes to discard before the next potential frame.
+    //----------------------------------------------------------------------------
     size_t Validate(size_t frameEnd) const override
     {
         const auto& clFrameBuffer = *pclMyBuffer;
@@ -74,10 +109,18 @@ class FramerBinaryBase : public FramerBase
     }
 
   public:
+    //----------------------------------------------------------------------------
+    //! \brief Reset the state of the Framer.
+    //----------------------------------------------------------------------------
     void ResetState() override {}
 
     using FramerBase::GetFrame;
 
+    //----------------------------------------------------------------------------
+    //! \brief Public interface to frame an OEM binary message - enforces metadata type.
+    //
+    //! \see GetFrame(unsigned char*, uint32_t, MetaDataBase&, bool)
+    //----------------------------------------------------------------------------
     [[nodiscard]] STATUS GetFrame(unsigned char* pucFrameBuffer_, uint32_t uiFrameBufferSize_, MetaDataStruct& stMetaData_)
     {
         return FramerBase::GetFrame(pucFrameBuffer_, uiFrameBufferSize_, stMetaData_, false);
