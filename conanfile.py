20a44d59a73a94c648e6dd3eed4fa4457b6ea30a
seanovens <sean.ovens@hexagon.com>
2026-08-05 12:46:34 -0600

    Restore: Switch to CMakeConfigDeps to fix VS 2026 build (#258)
    * Switch to CMakeConfigDeps to fix VS 2026 build
    
    * Update required Conan version to >=2.25.0
    
    ---------
    
    Co-authored-by: Riley Kinahan <riley.kinahan@hexagon.com>

--- conanfile.py
+++ conanfile.py
@@ -5,10 +5,10 @@ from pathlib import Path
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMake, cmake_layout, CMakeToolchain, CMakeDeps
+from conan.tools.cmake import CMake, cmake_layout, CMakeToolchain, CMakeConfigDeps
 from conan.tools.files import copy, rmdir
 
-required_conan_version = ">=2.4.0"
+required_conan_version = ">=2.25.0"
 
 class NovatelEdieConan(ConanFile):
     name = "novatel_edie"
@@ -122,7 +122,7 @@ class NovatelEdieConan(ConanFile):
         # Deploy CMake information for dependencies
         # Experimental generator (fixes VS 2026 builds)
         # https://github.com/conan-io/conan/issues/20148#issuecomment-4912499844
-        deps = CMakeDeps(self)
+        deps = CMakeConfigDeps(self)
         deps.generate()
 
         # Deploy any shared libraries
